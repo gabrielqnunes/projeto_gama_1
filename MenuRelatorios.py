ef7e3cc0d5e795e3c681ab049379c04744c5dedd
Gabriel Q. Nunes <gabrielq.nunes@luizalabs.com>
2022-09-13 13:57:33 -0300

    Criado relatório de últimos produtos vendidos

--- MenuRelatorios.py
+++ MenuRelatorios.py
@@ -1,3 +1,4 @@
+import json
 from Tela import Tela
 
 
@@ -11,11 +12,36 @@ class MenuRelatorios:
             print('======================================')
             print('||          MENU RELATÓRIOS         ||')
             print('======================================')
-            print('|| (1) Sair                         ||')
+            print('|| (1) Últimas vendas               ||')
+            print('|| (2) Voltar                       ||')
             print('======================================\n')
             entrada = input('Escolha uma opção:')
 
             if (entrada == '1'):
-                continuarExecutando = False
+                with open('Vendas.json', 'r') as file:
+                    vendasFile = json.load(file)
+                    vendas = vendasFile["vendas"]
 
-            # DESENVOLVER SOLUÇÃO AQUI
+                for venda in vendas:
+                    Tela.LimpaTela()
+                    print('======================================')
+                    preco_total = 0
+                    for item in venda:
+                        print('{} x {} = R$ {:.2f}'.format(
+                            item["preco"], item["quantidade"], item["preco"] * item["quantidade"]))
+                        preco_total = preco_total + \
+                            (item["preco"] * item["quantidade"])
+                    print('======================================')
+                    print('TOTAL: R$ {:.2f}'.format(preco_total))
+                    print('======================================')
+                    print('|| (1) Próxima compra                ||')
+                    print('|| (2) Voltar                        ||')
+                    print('======================================')
+                    continua_lista = ''
+                    while continua_lista not in ['1', '2']:
+                        continua_lista = input('Escolha um opção: ')
+                    if continua_lista == '2':
+                        break
+
+            if (entrada == '2'):
+                continuarExecutando = False
