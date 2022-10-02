cd324585b9425f93864dfd0abcb22f6341e9036f
Gabriel Q. Nunes <gabrielq.nunes@luizalabs.com>
2022-10-01 22:11:03 -0300

    Alterações gráficas e criação de rota para cadastrar produto

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 app = Flask(__name__)
@@ -19,6 +19,15 @@ def management():
 @app.route('/management/register')
 def register():
     return render_template('register.html')
+
+
+@app.route('/management/register/new', methods=['POST'])
+def register_new():
+    name = request.form['name']
+    price = request.form['price']
+    # FUNCIONALIDADE DE CADASTRAR PRODUTO
+    print(name)
+    return redirect('/management/register')
 
 
 if (__name__ == '__main__'):
