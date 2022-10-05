9443a4995ed40a5cebe12dd3f180ddfb234ee65c
Gabriel Q. Nunes <gabrielq.nunes@luizalabs.com>
2022-10-05 13:51:30 -0300

    Adicionado total de cada venda nos relatorios

--- app/services/Vendas.py
+++ app/services/Vendas.py
@@ -6,4 +6,15 @@ def get_vendas(path):
     vendas = None
     with open(path, 'r') as file:
         vendas = json.load(file)
+
+    print(vendas)
+
+    for venda in vendas:
+        total = 0
+        for item in venda:
+            print(item)
+            total += item['preco'] * item['quantidade']
+        venda.append(total)
+    print(vendas)
+
     return vendas
